5f50a15cd060c68efb4d0454b4479ea2aa4cc3e6
Meghadri-Koley <meghadrikoley9@gmail.com>
2022-11-15 22:28:33 +0530

    Navbar Icons fix

--- frontend/src/component/layout/Header/Header.js
+++ frontend/src/component/layout/Header/Header.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { ReactNavbar } from "overlay-navbar";
+import {MdAccountCircle } from "react-icons/md";
+import {MdSearch } from "react-icons/md";
+import {MdAddShoppingCart } from "react-icons/md";
 import logo from "../../../images/logo.png";
 
 const options = {
@@ -26,13 +29,15 @@ const options = {
   link1ColorHover: "#eb4034",
   link1Margin: "1vmax",
   profileIconUrl: "/login",
+  profileIcon:true,
   profileIconColor: "rgba(35, 35, 35,0.8)",
+  ProfileIconElement: MdAccountCircle, 
+  searchIcon:true,
   searchIconColor: "rgba(35, 35, 35,0.8)",
+  SearchIconElement:MdSearch,
+  cartIcon:true,
   cartIconColor: "rgba(35, 35, 35,0.8)",
-  profileIconColorHover: "#eb4034",
-  searchIconColorHover: "#eb4034",
-  cartIconColorHover: "#eb4034",
-  cartIconMargin: "1vmax",
+  CartIconElement:MdAddShoppingCart,
 };
 
 const Header = () => {
