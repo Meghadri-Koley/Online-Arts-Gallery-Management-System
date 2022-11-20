1af775b6e93277fcdc82ed72efc04e4498065d69
Meghadri-Koley <meghadrikoley9@gmail.com>
2022-11-20 12:17:41 +0530

    New Loader

--- frontend/src/component/layout/Loader/Loader.js
+++ frontend/src/component/layout/Loader/Loader.js
@@ -3,8 +3,27 @@ import "./Loader.css";
 
 const Loader = () => {
   return (
-    <div className="loading">
-      <div></div>
+    <div className="loading" id="loading">
+      {/* <div></div> */}  
+      {/* <h2>Mosaic Ripple<br>Loading Animation</h2> */}
+      <div class="mosaic-loader">
+        <div class="cell d-0"></div>
+        <div class="cell d-1"></div>
+        <div class="cell d-2"></div>
+        <div class="cell d-3"></div>
+        <div class="cell d-1"></div>
+        <div class="cell d-2"></div>
+        <div class="cell d-3"></div>
+        <div class="cell d-4"></div>
+        <div class="cell d-2"></div>
+        <div class="cell d-3"></div>
+        <div class="cell d-4"></div>
+        <div class="cell d-5"></div>
+        <div class="cell d-3"></div>
+        <div class="cell d-4"></div>
+        <div class="cell d-5"></div>
+        <div class="cell d-6"></div>
+      </div>
     </div>
     // <div class="preloader">
     //   <div class="preloader__square"></div>
